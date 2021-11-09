733803de54b53272734bc09f3930856635b9c4df
Tom Neudoerffer <tom@neudoerffer.com>
2021-11-08 22:28:14 -0500

    Added Ray struct, implemented basic ray casting to a background

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,79 @@
 mod color;
+mod ray;
 mod vec3;
 
 mod prelude {
     pub use crate::color::*;
+    pub use crate::ray::*;
     pub use crate::vec3::*;
 }
 
 use crate::prelude::*;
 
-const IMAGE_WIDTH: u32 = 256;
-const IMAGE_HEIGHT: u32 = 256;
+fn ray_color(r: &Ray) -> Color {
+    let unit_direction: Vec3 = r.dir.unit_vector();
+    let t = 0.5 * (unit_direction.y + 1.0);
+    (1.0 - t)
+        * Color {
+            x: 1.0,
+            y: 1.0,
+            z: 1.0,
+        }
+        + t * Color {
+            x: 0.5,
+            y: 0.7,
+            z: 1.0,
+        }
+}
 
 fn main() {
-    println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
+    // image
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width: u32 = 400;
+    let image_height = (image_width as f64 / aspect_ratio).round() as u32;
 
-    for j in (0..IMAGE_HEIGHT).rev() {
+    // camera
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3 {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+    };
+    let horizontal = Vec3 {
+        x: viewport_width,
+        y: 0.0,
+        z: 0.0,
+    };
+    let vertical = Vec3 {
+        x: 0.0,
+        y: viewport_height,
+        z: 0.0,
+    };
+    let lower_left_corner = origin
+        - horizontal / 2.0
+        - vertical / 2.0
+        - Vec3 {
+            x: 0.0,
+            y: 0.0,
+            z: focal_length,
+        };
+    // render
+    println!("P3\n{} {}\n255", image_width, image_height);
+
+    for j in (0..image_height).rev() {
         eprint!("\rScanlines Remaining: {}", j);
-        for i in 0..IMAGE_WIDTH {
-            let color = Color {
-                x: i as f64 / IMAGE_WIDTH as f64,
-                y: j as f64 / IMAGE_HEIGHT as f64,
-                z: 0.25,
+        for i in 0..image_width {
+            let u = i as f64 / (image_width as f64 - 1.0);
+            let v = j as f64 / (image_height as f64 - 1.0);
+            let r = Ray {
+                orig: origin,
+                dir: lower_left_corner + u * horizontal + v * vertical - origin,
             };
-            println!("{}", write_color(color));
+            let pixel_color = ray_color(&r);
+            println!("{}", write_color(pixel_color));
         }
     }
     eprint!("\nDone\n");
---
+++ src/ray.rs
@@ -0,0 +1,18 @@
+use crate::vec3::Vec3;
+
+pub type Point3 = Vec3;
+
+pub struct Ray {
+    pub orig: Point3,
+    pub dir: Vec3,
+}
+
+trait At {
+    fn at(&self, t: f64) -> Point3;
+}
+
+impl At for Ray {
+    fn at(&self, t: f64) -> Point3 {
+        self.orig + t * self.dir
+    }
+}
